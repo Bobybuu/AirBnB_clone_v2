8aadaccaa09fc03be0a5970a6af4a9c9d70778cd
Bobybuu <c43057772@gmail.com>
2024-05-02 03:42:42 -0700

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,7 +2,7 @@
 """ Fabric script (based on the file 2-do_deploy_web_static.py) that creates
 and distributes an archive to your web servers, using the function deploy: """
 
-
+execute:fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
 from fabric.api import *
 from datetime import datetime
 from os.path import exists
